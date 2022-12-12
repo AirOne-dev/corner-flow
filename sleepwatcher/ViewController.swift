fb76b62d192737c9ede3c4c49d85a0e8c3d363b1
Firologame <erwan1207@gmail.com>
2022-12-13 00:32:08 +0100

    Initial Commit

---
+++ sleepwatcher/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  sleepwatcher
+//
+//  Created by Erwan Martin on 13/12/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
